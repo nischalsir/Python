f7ec513dcd9e3369975247ac605aa3e82886c88b
nischalsir <pandenishchal@gmail.com>
2024-12-26 08:20:19 +0545

    `Added print statement and user input functionality to day1.py`

--- day1.py
+++ day1.py
@@ -1,1 +1,46 @@
-print("hello world!")
+print("Hello world /n")
+
+# Get user input for name and print a personalized greeting.
+
+name = input("Enter your name: ")
+print("Hello," + name + "!")
+
+# Variable and datatypes
+
+age = 22
+print("Your age is", age)
+
+name ="Nischal"
+print("Helllo," + name + "!")
+
+height = 1.75
+print("Your height is", height)
+
+isStudent = False
+print("You are a student:", isStudent)
+
+#basic arithmetic operations
+
+a=10
+b=5
+
+print("Addition:", a+b)
+
+print("Subtraction:", a-b)
+
+print("Multiplication:", a*b)
+
+print("Division:", a/b)
+
+print("Modulus:", a%b)
+
+#if else statement
+
+num= int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive")
+    elif num < 0:
+        print("The number is negative")
+        else:
+            print("The number is zero")
